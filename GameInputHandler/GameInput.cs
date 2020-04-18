f29e2433cf2104a7ece0d5597fdfe52dd432d58e
Lukas Milar <lukasmilar87@gmail.com>
2020-04-18 15:09:35 +0200

    Added new project for user input translation

---
+++ GameInputHandler/GameInput.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameInputHandler
+{
+    [Flags]
+    public enum GameKey
+    {
+        None = 0,
+        Escape = 1 << 0,
+        Up = 1 << 1,
+        Left = 1 << 2,
+        Down = 1 << 3,
+        Right = 1 << 4,
+        Action = 1 << 5,
+        Back = 1 << 6,
+        Action2 = 1 << 7
+    }
+    public class GameInput
+    {
+    }
+}
