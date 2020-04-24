a83216d2e4df488ca6d8c7d1dadd0863cff0c9ca
Lukas Milar <lukasmilar87@gmail.com>
2020-04-24 11:14:49 +0200

    Added the base for the rest of the submenus

---
+++ WPFGame/UI/MainMenu/SettingsSubMenu/Controls/ControlsMenu.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFGame.UI.MainMenu.SettingsSubMenu.Controls
+{
+    /// <summary>
+    /// Interaction logic for ControlsMenu.xaml
+    /// </summary>
+    public partial class ControlsMenu : UserControl
+    {
+        public ControlsMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFGame/UI/MainMenu/SettingsSubMenu/Graphics/GraphicsMenu.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFGame.UI.MainMenu.SettingsSubMenu.Graphics
+{
+    /// <summary>
+    /// Interaction logic for GraphicsMenu.xaml
+    /// </summary>
+    public partial class GraphicsMenu : UserControl
+    {
+        public GraphicsMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
