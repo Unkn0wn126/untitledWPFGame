c5e4ff7caa718698830d48b8d639042be6a43b78
Lukas Milar <lukasmilar87@gmail.com>
2020-04-27 18:14:59 +0200

    Implemented enemy battle logic

--- Engine/Models/Components/Script/AIBattleScript.cs
+++ Engine/Models/Components/Script/AIBattleScript.cs
@@ -18,7 +18,35 @@ namespace Engine.Models.Components.Script
         {
             int initialAction = _rnd.Next(Enum.GetValues(typeof(MovementType)).Length);
             _ownerState.MovementType = (MovementType)initialAction;
+            SetAttackDirection(_ownerState.MovementType);
+            SetAttackType(_ownerState.AttackDirection);
             _ownerState.TurnDecided = true;
+        }
+
+        private void SetAttackType(AttackDirection direction)
+        {
+            if (direction == AttackDirection.None)
+            {
+                _ownerState.AttackType = AttackType.None;
+            }
+            else
+            {
+                int attackType = _rnd.Next(1, Enum.GetValues(typeof(AttackType)).Length);
+                _ownerState.AttackType = (AttackType)attackType;
+            }
+        }
+
+        private void SetAttackDirection(MovementType movementType)
+        {
+            if (movementType == MovementType.Heal)
+            {
+                _ownerState.AttackDirection = AttackDirection.None;
+            }
+            else
+            {
+                int attackDirection = _rnd.Next(1, Enum.GetValues(typeof(AttackDirection)).Length);
+                _ownerState.AttackDirection = (AttackDirection)attackDirection;
+            }
         }
     }
 }
